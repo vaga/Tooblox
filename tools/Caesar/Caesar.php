376cd253e3c9c3af5f60db944257f51815bc46d6
Vagaweb <fabien.casters@gmail.com>
2013-08-06 14:03:39 +0200

    Faute d'orthographe.

--- tools/Caesar/Caesar.php
+++ tools/Caesar/Caesar.php
@@ -8,7 +8,7 @@ class Caesar extends AbstractTool {
 
     function __construct() {
 
-		$this->set('_title', 'Cesar');
+        $this->set('_title', 'Caesar');
         $this->set('_description', 'Cesare code and decode tool.');
         $this->set('_keywords', 'cesare, encode, decode, tool');
         $this->set('_author', 'Fabien Casters');
